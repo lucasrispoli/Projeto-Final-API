fd318514bc3e3b14fd83bf10480302b880a9ad48
unknown <leandrofreitas.dev@gmail.com>
2025-05-29 13:03:36 -0300

    package

---
+++ projetofinal/src/main/java/org/serratec/backend/entity/Produto.java
@@ -0,0 +1,42 @@
+package org.serratec.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Integer valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getValor() {
+		return valor;
+	}
+
+	public void setValor(Integer valor) {
+		this.valor = valor;
+	}
+
+}
---
+++ projetofinal/src/main/java/org/serratec/backend/service/ClienteService.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.service;
+
+import org.serratec.backend.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+
+}
